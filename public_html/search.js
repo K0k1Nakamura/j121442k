bcf2be12d3be9c3fe7262c739d7c74576b8094fe
k00ook17@gmail.com <k.nakamura@z8.keio.jp>
2014-11-10 16:46:18 +0900

    gazoudaseta

--- public_html/search.js
+++ public_html/search.js
@@ -14,11 +14,18 @@ $(function(){
 				var $div = $('<div></div>').addClass('col-sm-4 col-lg-4　col-md-4');
 
 				// FIXME: 汚い。力技すぎる。
-				$div.html('<div class="thumbnail"><img src="http://placehold.it/320x150" alt=""><div class="caption"><h4 class="pull-right">¥'+
+				$div.html('<div class="thumbnail"><div style="height:150px;width:320px;"><img src="pic/' +
+					res[i].pic + '" style="max-height: 150px; width: 320px;"></div><div class="caption"><h4 class="pull-right">¥'+
 					res[i].price + '</h4><h4><a href="detail.php?id=' +
 					res[i].id + '">' +
-					res[i].name + '</a></h4><p>' +
-					res[i].comment + '</p></div><div class="ratings"><p class="pull-right">15 reviews</p><p><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span></p></div></div>');
+					res[i].name + '</a></h4><h6>' +
+					res[i].author + '</h6><h6>'　+
+					res[i].class + '</h6><h6>' + 
+					res[i].university + ' ' + 
+					res[i].faculty + ' ' + 
+					res[i].department + '</h6>');
+					 // +res[i].comment + '</p>');
+				// </div><div class="ratings"><p class="pull-right">15 reviews</p><p><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span></p></div></div>
 				
 				$('#text_list').append($div);
 
